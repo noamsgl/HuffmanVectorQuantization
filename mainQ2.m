5ed6ec73237f68bc69adf4f3ede6e90c34ee779c
noam <noamsi@post.bgu.ac.il>
2022-01-23 22:22:35 +0200

    added kmeans

---
+++ mainQ2.m
@@ -0,0 +1,35 @@
+
+%% Question 2.1
+
+%% Clear environment
+close all
+clear
+clc
+
+%% Initialize parameters
+Q = 2^4;  % num_levels, [2^2, ..., 2^10]
+N = 2;  % num_samples in each group, [1, 2, 3]
+
+%% Load data
+load('data.mat');
+
+
+%% Crop to Multiple of N and Flatten
+data = data(:,1:end - mod(width(data), N));
+n_channels = height(data);
+n_samples = width(data);
+data = reshape(data, [1, n_channels * n_samples]);
+
+%% Train Vector Quantization Codebook
+
+codebook = trainVQ(data, Q, N);
+reshaped_data = reshape(data.', N, []).';
+scatter(reshaped_data(:,1), reshaped_data(:,2))
+scatter(codebook(:,1), codebook(:,2))
+title("2.2")
+
+
+fprintf("success\n")
+
+
+
---
+++ trainVQ.m
@@ -0,0 +1,15 @@
+function C = trainVQ(data, Q, N)
+% calculate VQ codebook (mapping from code words to 
+% Q is number of quantization levels
+% N is number of scalars in each block
+if ~(mod(numel(data), N) == 0)
+    error('data size is not a multiple of N')
+end
+
+% Reshape data to column 
+data = reshape(data.', N, []).';
+
+% Get k cluster centroid locations
+[idx, C] = kmeans(data, Q);
+
+end
